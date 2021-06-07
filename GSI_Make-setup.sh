cd7f0ce4ad31106f585af5bcbff5771a47182f30
BlueSword03 <62334979+BlueSword03@users.noreply.github.com>
2021-06-07 22:47:34 +0800

    Update GSI_Make-setup.sh

--- GSI_Make-setup.sh
+++ GSI_Make-setup.sh
@@ -2,4 +2,5 @@
 sudo -E apt-get -qq install git wget install vim
 git clone --recurse-submodules https://github.com/BlueSword03/ErfanGSIs_MIUI_Only.git ~/ErfanGSIs_MIUI_Only
 curl -sL https://git.io/file-transfer | sh
-git clone https://github.com/BlueSword03/Scripts_For_ROM_Build.git
+git clone https://github.com/BlueSword03/Scripts_For_ROM_Build.git ~/Script
+sudo chmod 777 ~/Script/GSI_Make-run.sh
