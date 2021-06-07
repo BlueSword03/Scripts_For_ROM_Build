011805457c8992f2ca13e5a508293e7a3084315b
BlueSword03 <62334979+BlueSword03@users.noreply.github.com>
2021-06-07 23:09:10 +0800

    Fixed again

--- GSI_Make-run.sh
+++ GSI_Make-run.sh
@@ -3,7 +3,6 @@ cd ~/ErfanGSIs_MIUI_Only
 bash setup.sh
 ./url2GSI.sh -b https://udomain.dl.sourceforge.net/project/xiaomi-eu-multilang-miui-roms/xiaomi.eu/MIUI-WEEKLY-RELEASES/21.4.14/xiaomi.eu_multi_HMNote7_21.4.14_v12-10.zip MIUI
 cd ~/ErfanGSIs_MIUI_Only/output
-mkdir out
-zip -r out/GSI.zip ./*
+zip -r ~/ErfanGSIs_MIUI_Only/GSI.zip ./*
 cd ~/
-./transfer wet ~/ErfanGSIs_MIUI_Only/output/GSI.zip
+./transfer cow ~/ErfanGSIs_MIUI_Only/GSI.zip
